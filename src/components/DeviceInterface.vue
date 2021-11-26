833bc3fa9a58c0bbf04836c6bbf9bf6b8ce6c894
jchou25101 <jchou22@uwo.ca>
2021-11-26 15:25:08 -0500

    Placeholder Device Interfaces
    Everything so far is dummy data and placeholder GUI
    
    -DeviceManager.vue dynamically displays and manages the full list of device interfaces
    -DeviceInterface.vue provides all the GUI functionality for a single device

---
+++ src/components/DeviceInterface.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Placeholder Device</h2>
+        <p>Interface stuff goes here</p>
+    </div>
+
+</template>
--- src/components/DeviceManager.vue
+++ src/components/DeviceManager.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <h1>Connected Devices</h1>
-        <ul id="dynamic-list"></ul>
+        <ul id="dynamic-list">
+          <li v-for="device in devices" :key="device.IP">
+            <DeviceInterface/>
+          </li>
+        </ul>
 
         <button v-on:click="addItem()">add item</button>
         <button v-on:click="removeItem()">remove item</button>
@@ -10,21 +14,37 @@
 </template>
 
 <script>
+import DeviceInterface from './DeviceInterface.vue'
+
 export default {
   name: 'DeviceManager',
   // define methods under the `methods` object
   methods: {
     addItem: function (){
         var ul = document.getElementById("dynamic-list");
-        var li = document.createElement("li");
-        li.setAttribute('id','New Device');
-        li.appendChild(document.createTextNode('New Device'));
+        var li = document.createElement("DeviceInterface");
+        //var li = document.createElement("li");
+        //li.setAttribute('id','New Device');
+        //li.appendChild(document.createTextNode('New Device'));
         ul.appendChild(li);
     },
     removeItem: function (){
         var ul = document.getElementById("dynamic-list");
         var item = document.getElementById('New Device');
         ul.removeChild(item);
+    }
+  },
+  components: {
+    DeviceInterface
+  },
+  data(){
+    return{
+      devices:
+      [
+        {IP:"0", name:"Placeholder Device"},
+        {IP:"1", name:"Placeholder Device"},
+        {IP:"2", name:"Placeholder Device"},
+      ]
     }
   }
 }
