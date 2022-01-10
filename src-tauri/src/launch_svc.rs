3870bd2d593efaf01deaf5dd4c97427ef55e287b
Axat09 <40310648+Axat09@users.noreply.github.com>
2022-01-10 13:53:05 -0500

    launch svc from client

---
+++ src-tauri/src/launch_svc.rs
@@ -0,0 +1,26 @@
+use tauri::{command};
+
+use super::{packet::*,remote_conn_svc::*};
+#[command]
+pub async fn launch() -> String{
+    
+    let my_packet = Packet {
+        packet_id: s!["OPENLINK"],
+        version: 1,
+        cmd_type: 69,
+        timestamp: std::time::SystemTime::now(),
+        payload: vec!["Launch command from Client".to_string()]
+    }
+    //println! ("Data Sent");
+
+    let data = send_packet(my_packet).await;
+
+    data.payload[0].clone();
+}
+
+async fn send_packet(my_packet: Packet) -> Packet {
+
+    let data = send("127.0.0.1:6007".parse().unwrap(), my_packet).await;
+    let data = data.unwrap();
+    data
+}
