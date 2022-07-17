97e1e9d4a0eb0f485d09ba00ca1dcd028c451527
Matt Holden <m@hold3n.io>
2022-07-17 17:56:49 -0400

    chore: update dependencies, separate repo

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,18 @@ import App from './App.vue'
 
 import { createPinia } from 'pinia'
 import { Quasar } from 'quasar'
-import quasarUserOptions from './quasar-user-options'
 
 import router from './services/router'
+
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+
+import 'quasar/src/css/index.sass'
 
 createApp(App)
   .use(router)
   .use(createPinia())
-  .use(Quasar, quasarUserOptions)
+  .use(Quasar, {
+    plugins: {},
+  })
   .mount('#app')
